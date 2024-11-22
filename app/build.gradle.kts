6e2da8ec2b65eb2443d18c98b5b24bfa104060ab
Philipp Lackner <mail@pl-coding.com>
2024-11-22 11:56:24 +0100

    ConstraintLayout demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
 
     implementation(libs.androidx.recyclerview)
 
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.constraintlayout.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
