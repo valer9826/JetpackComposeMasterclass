da1d2880201cf12a1c019b74c2cb6adfddd38178
Philipp Lackner <mail@pl-coding.com>
2024-11-27 17:27:59 +0100

    Modifier factories

--- app/src/main/java/com/plcoding/jetpackcomposemasterclass/measurements/ComplexCustomModifiers.kt
+++ app/src/main/java/com/plcoding/jetpackcomposemasterclass/measurements/ComplexCustomModifiers.kt
@@ -14,13 +14,69 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.node.CompositionLocalConsumerModifierNode
+import androidx.compose.ui.node.LayoutModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.node.currentValueOf
+import androidx.compose.ui.node.invalidateMeasurement
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.plcoding.jetpackcomposemasterclass.ui.theme.JetpackComposeMasterclassTheme
+
+fun Modifier.improvedNegativePadding(horizontal: Dp): Modifier {
+    return this.then(NegativePaddingElement(horizontal))
+}
+
+data class NegativePaddingElement(
+    private val horizontal: Dp
+): ModifierNodeElement<NegativePaddingNode>() {
+
+    override fun create(): NegativePaddingNode {
+        return NegativePaddingNode(horizontal)
+    }
+
+    override fun update(node: NegativePaddingNode) {
+        node.horizontal = horizontal
+    }
+}
+
+class NegativePaddingNode(
+    var horizontal: Dp
+): LayoutModifierNode, Modifier.Node(), CompositionLocalConsumerModifierNode {
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+        val density = currentValueOf(LocalDensity)
+        val px = with(density) {
+            horizontal.roundToPx()
+        }
+
+        val placeable = measurable.measure(
+            constraints = constraints.copy(
+                minWidth = constraints.minWidth + 2 * px,
+                maxWidth = constraints.maxWidth + 2 * px
+            )
+        )
+
+        return layout(placeable.width, placeable.height) {
+            placeable.place(0, 0)
+        }
+    }
+}
 
 @Composable
 fun Modifier.negativePadding(horizontal: Dp): Modifier {
@@ -49,18 +105,23 @@ fun MyList(modifier: Modifier = Modifier) {
             .fillMaxSize()
             .padding(horizontal = 16.dp)
     ) {
+        var toggle by remember {
+            mutableStateOf(true)
+        }
         Text(
             text = "This is item 1",
             modifier = Modifier
                 .clickable {
-
+                    toggle = !toggle
                 }
                 .fillMaxWidth()
                 .padding(vertical = 16.dp)
+                .improvedNegativePadding(
+                    if(toggle) 16.dp else 32.dp)
         )
         HorizontalDivider(
             modifier = Modifier
-                .negativePadding(16.dp)
+                .improvedNegativePadding(16.dp)
         )
         Text(
             text = "This is another item",
@@ -73,7 +134,7 @@ fun MyList(modifier: Modifier = Modifier) {
         )
         HorizontalDivider(
             modifier = Modifier
-                .negativePadding(16.dp)
+                .improvedNegativePadding(16.dp)
         )
         Text(
             text = "And another one",
