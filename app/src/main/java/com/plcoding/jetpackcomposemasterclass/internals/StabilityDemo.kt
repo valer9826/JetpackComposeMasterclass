72f492372e26c32e75c7ae9aa46e394c94b178ce
Philipp Lackner <mail@pl-coding.com>
2024-12-07 13:02:39 +0100

    Stability Immutability Demo

--- app/src/main/java/com/plcoding/jetpackcomposemasterclass/internals/StabilityDemo.kt
+++ app/src/main/java/com/plcoding/jetpackcomposemasterclass/internals/StabilityDemo.kt
@@ -4,9 +4,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 
-@Immutable
 data class Person(
     val name: String,
     val age: Int,
