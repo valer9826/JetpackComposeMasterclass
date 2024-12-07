5362c8052750720006fac3a6d22dc6fe231cd54b
Philipp Lackner <mail@pl-coding.com>
2024-12-07 12:58:20 +0100

    Stability Immutability Demo

---
+++ app/src/main/java/com/plcoding/jetpackcomposemasterclass/internals/StabilityDemo.kt
@@ -0,0 +1,29 @@
+package com.plcoding.jetpackcomposemasterclass.internals
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+
+@Immutable
+data class Person(
+    val name: String,
+    val age: Int,
+    val list: List<Int>
+)
+
+val list1 = listOf(1, 2, 3)
+val list2 = listOf(1, 2, 3, 4)
+val result = list1 == list2
+
+@Composable
+fun StabilityDemo(
+    person: Person,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = "${person.name} is ${person.age} years old.",
+        modifier = modifier
+    )
+}
